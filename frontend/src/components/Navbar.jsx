549ddec4bf74c152dde2967591c71442f22507f4
Gavin <gavinctraylor@gmail.com>
2024-12-02 21:14:18 -0500

    prettier plugin and file layout

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+	return (
+		<>
+			<nav className="bg-slate-400">
+				<div className="">Qualve</div>
+				<input className="" placeholder="Search..." type="text" />
+				<div className="">User Profile Button</div>
+			</nav>
+		</>
+	);
+}
