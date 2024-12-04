8cd21b38a650b81aa7e810f74eebc5b355f8733c
Gavin <gavinctraylor@gmail.com>
2024-12-03 20:09:47 -0500

    react router config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import Navbar from "./components/common/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages.Home";
+import NavBar from "./components/Navbar";
 
 function App() {
-	return (
-		<>
-			<Navbar />
-			<div className="bg-blue-600">Hello World!</div>
-		</>
-	);
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <div className="bg-blue-600">Hello World!</div>
+    </>
+  );
 }
 
 export default App;
