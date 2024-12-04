6d606327d555d2457a3e65e691c09c5a5799383a
Gavin <gavinctraylor@gmail.com>
2024-12-03 21:37:43 -0500

    file config - routes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 export default function Navbar() {
-	return (
-		<>
-			<nav className="bg-slate-400">
-				<div className="">Qualve</div>
-				<input className="" placeholder="Search..." type="text" />
-				<div className="">User Profile Button</div>
-			</nav>
-		</>
-	);
+  return (
+    <>
+      <nav className="bg-slate-400">
+        <div className="bg-slate-300">Navbar</div>
+        {/* navbar component */}
+        {/* searchbar component component */}
+        {/* profile component */}
+      </nav>
+    </>
+  );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div></div>;
+  return <div className="bg-blue-300">This is the home page</div>;
 }
